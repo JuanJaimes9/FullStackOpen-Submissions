8bb3e487e014d049ac099d06a63771bbe9ef21fc
Juan Jaimes <juandiegojaimesp99@gmail.com>
2024-07-23 22:33:24 -0500

    Revert "courseInfo part2"
    This reverts commit 8afe2def33c99e2bd4801b7d6723e40964b9cb78.

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,52 +1,34 @@
-import Course from "./components/Course";
-
+import { Header } from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
 const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
 
-  return <Course courses={courses} />;
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
 };
 
 export default App;
